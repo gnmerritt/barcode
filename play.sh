77c403d6e2ec5599871173efedf8225487e3ded4
Nathan Merritt <nathan@gnmerritt.net>
2024-09-06 15:21:20 -0600

    play script takes an opponent argument

--- play.sh
+++ play.sh
@@ -2,17 +2,19 @@
 cd "$(dirname "$0")"
 
 REMOTE=batista.local
+opponent=${1:-barcode-checkpoint2}
 
 rsync -Calpr src/ ${REMOTE}:~/sources/barcode/src/
+rsync -Calpr Cargo* ${REMOTE}:~/sources/barcode/
 
 # give the build 3 seconds and then launch VNC here while SSH is open
 ( sleep 5 ; vnc-viewer -Scaling 175 ${REMOTE}:5900 ) &
 
-ssh ${REMOTE} << 'EOF'
+ssh ${REMOTE} << EOF
   cd ~/sources/barcode/
   cargo build --release --target=i686-pc-windows-gnu
   cp target/i686-pc-windows-gnu/release/barcode.exe ~/.scbw/bots/barcode/AI/barcode.exe
 
-  export PATH=${PATH}:~/.pyenv/versions/bw-docker/bin/  
-  scbw.play --bots "barcode" "barcode-checkpoint"
+  ~/.pyenv/versions/bw-docker/bin/scbw.play \
+    --bots "barcode" "${opponent}"
 EOF
